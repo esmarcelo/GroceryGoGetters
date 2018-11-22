8a8de146fb9b14ab834652c0889a1486d2c278e1
ericsmarcelo <emarc003@ucr.edu>
2018-11-22 06:56:25 -0500

    Front End Thanksgiving

--- Grocery/src/app/profile/profile.component.ts
+++ Grocery/src/app/profile/profile.component.ts
@@ -9,6 +9,9 @@ export class ProfileComponent implements OnInit {
 
   constructor() { }
 
+  editProfile(first: string, last: string, add1: string, add2: string, city: string, state: string, phone: string, zip: number) {
+    console.log(first + ' ' + last + ' ' + add1 + ' ' + add2 + ' ' + city + ' ' + state + ' ' + phone + ' ' + zip);
+  }
   ngOnInit() {
   }
 
--- Grocery/src/app/schedule/schedule.component.ts
+++ Grocery/src/app/schedule/schedule.component.ts
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleComponent implements OnInit {
 
+
   constructor() { }
+
+  scheduleDelivery(del: string, freq: string, first: string, last: string,
+    add: string, add2: string, ct: string, state: string, zip: number) {
+    console.log('Delivery Scheduled for' + del + ' ' + freq
+    + ' ' + first + ' ' + last + ' ' + add + ' ' + add2
+    + ' ' + ct + ' ' + state + ' ' + zip);
+  }
 
   ngOnInit() {
   }
