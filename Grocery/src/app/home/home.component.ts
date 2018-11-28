878456ca3f05aa9442e6690996791079036ea8af
ericsmarcelo <emarc003@ucr.edu>
2018-11-28 17:47:13 -0500

    http calls

--- Grocery/src/app/home/home.component.ts
+++ Grocery/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  data;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('http://ec2-3-16-151-233.us-east-2.compute.amazonaws.com:8080/GroceryGoGetters/users').subscribe(data => {
+      console.log(data);
+      this.data = data;
+    });
   }
 
 }
