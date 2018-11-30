f5b9d32e10032aa1dfb0feb5b2fa8b5b65a855bf
ericsmarcelo <emarc003@ucr.edu>
2018-11-29 21:28:23 -0500

    posting customer

--- Grocery/src/app/schedule/schedule.component.ts
+++ Grocery/src/app/schedule/schedule.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-schedule',
@@ -7,18 +8,39 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent implements OnInit {
+  recurrence;
+  carts;
+  cid;
 
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
-  constructor(private http: HttpClient) { }
+  scheduleDelivery() {
+    console.log('Delivery Scheduled');
+    this.http.post('http://ec2-3-16-151-233.us-east-2.compute.amazonaws.com:8080/GroceryGoGetters/createEvent', {
+      'cart_id': {'cart_id': this.cid },
+      'event_created': '',
+      'recur_id': {'recur_id': this.recurrence},
+      'user_id': { 'user_id': this.authService.getId() }
+    }).subscribe(res => {
+      console.log(res);
+    });
+  }
 
-  scheduleDelivery(del: string, freq: string, first: string, last: string,
-    add: string, add2: string, ct: string, state: string, zip: number) {
-    console.log('Delivery Scheduled for' + del + ' ' + freq
-    + ' ' + first + ' ' + last + ' ' + add + ' ' + add2
-    + ' ' + ct + ' ' + state + ' ' + zip);
+  setValue(recurrence) {
+    this.recurrence = recurrence;
+    console.log(this.recurrence);
+  }
+
+  chooseCart(cid) {
+    this.cid = cid;
+    console.log(cid);
   }
 
   ngOnInit() {
+      this.http.get('http://ec2-3-16-151-233.us-east-2.compute.amazonaws.com:8080/GroceryGoGetters/shoppingcarts/'
+     + this.authService.getId()).subscribe(cart => {
+      this.carts = cart;
+    });
   }
 
 }
